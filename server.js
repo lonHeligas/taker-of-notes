2e86e1bf288e6706ac90437b0430466c0a7bdb32
Lon Heligas <lon.heligas@gmail.com>
2023-02-02 09:33:34 -0600

    cleaned up comments and added README file

--- server.js
+++ server.js
@@ -12,14 +12,15 @@ app.use(express.static('public'));
 
 
 
-// todo GET html route for /notes (returns the notes.html file)
+// * GET html route for /notes (returns the notes.html file)
+
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
 app.get('/api/notes', (req, res) => {  
   const stringData = fs.readFileSync('./db/db.json');
   res.json(JSON.parse(stringData));
 })
-  // res.send("a word")
+  
 
 app.post('/api/notes', (req, res) => {
 
@@ -31,16 +32,16 @@ app.post('/api/notes', (req, res) => {
   const dbData = JSON.parse(fs.readFileSync('./db/db.json'));
 
 
-  // pushes the new note data into the database
+  // * pushes the new note data into the database
   dbData.push(newNote);
   // * console.log(dbData);
   
-  // write the data to the json file
+  // * write the data to the json file
   fs.writeFileSync('./db/db.json', JSON.stringify(dbData , null , 2));
   res.send(newNote);
 })
 
-
+// * deletes the note from the page once the user has clicked a notes's trash can icon
 app.delete('/api/notes/:id', (req, res) => {
 
   const dbData = JSON.parse(fs.readFileSync('./db/db.json'));
@@ -52,13 +53,13 @@ app.delete('/api/notes/:id', (req, res) => {
 
   if (deleted) {
 
-    console.log(deleted);
+    // * console.log(deleted);
 
     const newDbData = dbData.filter(note => note.id != id)  
     fs.writeFileSync('./db/db.json', JSON.stringify(newDbData , null , 2));      
     res.status(200).json(deleted);
   } else {
-    console.log(deleted);
+    // * console.log(deleted);
     res
       .status(404)
       .json({ message: 'The record you are looking for does not exist'})
@@ -71,16 +72,8 @@ app.delete('/api/notes/:id', (req, res) => {
 
 
 
-// * GET API route for /api/notes should read the db.json file and return all saved notes as JSON
-// * POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. Figure out a way to give each note a unique id when it's saved (look into Npm packages that could do this for you) That should be uuid that does that
+// * GET html route for * (should return the index.html file)
 
-// * receive the notes from the request body
-// * assign an id
-// * add it do the db.json file
-// * return the new note to the client
-
-
-// todo GET html route for * (should return the index.html file)
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
 app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT} 🚀`));
